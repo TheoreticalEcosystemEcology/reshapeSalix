c86ca854af7779aeddecb68644155dad7c02d7be
Kevin Cazelles <kevin.cazelles@um2.fr>
2017-05-18 16:51:02 -0400

    code to get kopelke data in alien is completed

--- lib/alienize.r
+++ lib/alienize.r
@@ -1,5 +1,7 @@
 ## packages needed
 library(magrittr)
+library(reshape2)
+library(stringi)
 devtools::install_github("TheoreticalEcosystemEcology/alien")
 devtools::install_github("letiR/letiRmisc")
 
@@ -29,38 +31,62 @@ for (i in 1:nrow(df_site)){
   id <- which(tmp==3)
   df_site$ID[i] <- df_site2$ID[id]
 }
-
-
+## assigning an unique id to sites in df_interact
 df_interact$ID_site <- ""
 for (i in 1:nrow(df_interact)){
   id <- which(as.character(df_site$REARING_NUMBER) == as.character(df_interact$REARING_NUMBER[i]))
   df_interact$ID_site[i] <- df_site$ID[id]
 }
 
-
 tmp <- data.frame(
     IdSite = rep(df_interact$ID_site, 3),
     IDSp = c(as.character(df_interact$RSAL),
       as.character(df_interact$RGALLER),
-      df_interact$RPAR)
+      df_interact$RPAR),
+    stringsAsFactors = FALSE
     )
 id <- which(tmp$IDSp == "none")
-##
-idObs <- tmp[-id, ]
-## Mettre les individus... :)
-# siteEnv <-
+
+idObs <- tmp[-id, ] %>% unique
+## TODO Mettre les individus pour Salix... :)
 
 
 
 
-####----- InteractPair
+####----- interactPair
 
 # interactPair
-# df_interact
+# df_interact (2 times!)
+rsal <- df_interact$RSAL %>% as.character
+rgal <- df_interact$RGALLER %>% as.character
+rpar <- df_interact$RPAR %>% as.character
+interactPair <- data.frame(
+  idFrom = c(rsal, rgal),
+  idTo = c(rgal, rpar),
+  stringsAsFactors = FALSE
+  )
+##-- Add frequence as value
+conc <- paste0(interactPair$idFrom, interactPair$idTo)
+tbint <- table(conc)
+# Unique pair of interactip
+interactPair <- unique(interactPair)
+interactPair$value <- NA
+for (i in 1:nrow(interactPair)){
+  tmpconc <- paste0(interactPair$idFrom[i],interactPair$idTo[i])
+  id <- which(names(tbint) == tmpconc)
+  interactPair$value[i] <- tbint[id]
+}
+interactPair <- interactPair[-which(interactPair$idTo == "none"),]
 
 
 
 ####----- INFO SITES
+sitetmp <- df_site2[,c(6,2:5,1)]
+## removing non-ascii character
+sitetmp$SITE <- stri_trans_general(sitetmp$SITE, "latin-ascii")
+sitetmp$REGION <- stri_trans_general(sitetmp$REGION, "latin-ascii")
+siteEnv <- melt(sitetmp, id.vars = names(sitetmp)[1])
+# names(siteEn)
 
 
 
@@ -75,21 +101,18 @@ traiSP <- data.frame(
   genus = c(rep("Salix", nrow(df_salix)),
     df_galler$GENUS,
     df_parasit$GENUS),
-  species = c(df_salix$SPECIES,
-    df_galler$SPECIES,
-    df_parasit$FULL_NAME %>% keepWords(2) %>% unlist)
+  species = c(df_salix$SPECIES %>% stri_trans_general("latin-ascii"),
+    df_galler$SPECIES %>% stri_trans_general("latin-ascii"),
+    df_parasit$FULL_NAME %>% keepWords(2) %>% unlist %>% stri_trans_general("latin-ascii")),
+  stringsAsFactors = FALSE
   )
 
 
 
 
-#@param siteEnv A matrix or a data frame where each column is a descriptor of the sites. TODO: siteEnv should cover the possibility that environmental variables could be taken at several times - link to idTime in idObs?.
-#@param traitSp A matrix or a data frame where each column is a trait characterizing all species. The first column is a unique identifier of the species documented in \code{idObs} data.frame.
-#@param traitInd
-
-
-
 ####----- SAVING THA DATA
-
-# as.alienData(idObs, )
-# save(x, file="x.Rda")
+kopelke <- alien::as.alienData(idObs, interactPair, siteEnv = siteEnv, traitSp = traiSP)
+save(kopelke, file='kopelke.rda', compress='xz', ascii=TRUE)
+# devtools::use_data(sal, salix)
+# save(salix, file="salix.Rda")
+# save(bartomeus, file='bartomeus.rda', compress='xz')
