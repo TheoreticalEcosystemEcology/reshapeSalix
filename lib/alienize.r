62b7b52f10d9282ed7f60e89ff2a4cb74ca74981
Kevin Cazelles <kevin.cazelles@um2.fr>
2017-06-11 00:29:56 -0400

    formating data using the new alienData function

--- lib/alienize.r
+++ lib/alienize.r
@@ -1,9 +1,11 @@
 ## packages needed
 library(magrittr)
+library(letiRmisc)
 library(reshape2)
 library(stringi)
-devtools::install_github("TheoreticalEcosystemEcology/alien")
-devtools::install_github("letiR/letiRmisc")
+library(taxize)
+# devtools::install_github("TheoreticalEcosystemEcology/alien")
+# devtools::install_github("letiR/letiRmisc")
 
 ## Use formated data
 source("lib/format4R.r")
@@ -18,101 +20,133 @@ df_salix_galler <- readRDS("rdata/df_salix_galler.rds")
 
 
 
-####------ idObs
 
-df_site2 <- df_site[, c('SITE', 'NDECDEG', 'EDECDEG', 'COUNTRY', 'REGION')] %>% unique
-df_site2$ID <- paste0("site", 1:nrow(df_site2))
-## assigning an unique id to sites
-df_site$ID <- ""
-for (i in 1:nrow(df_site)){
-  tmp <- (df_site$SITE[i] == df_site2$SITE) +
-    (df_site$NDECDEG[i] == df_site2$NDECDEG) +
-    (df_site$EDECDEG[i] == df_site2$EDECDEG)
-  id <- which(tmp==3)
-  df_site$ID[i] <- df_site2$ID[id]
-}
-## assigning an unique id to sites in df_interact
-df_interact$ID_site <- ""
-for (i in 1:nrow(df_interact)){
-  id <- which(as.character(df_site$REARING_NUMBER) == as.character(df_interact$REARING_NUMBER[i]))
-  df_interact$ID_site[i] <- df_site$ID[id]
-}
+######################### BUILDING dfNodes
 
-tmp <- data.frame(
-    IdSite = rep(df_interact$ID_site, 3),
-    IDSp = c(as.character(df_interact$RSAL),
-      as.character(df_interact$RGALLER),
-      df_interact$RPAR),
-    stringsAsFactors = FALSE
-    )
-id <- which(tmp$IDSp == "none")
+## add species
+df_parasit$SPECIES <- keepWords(df_parasit$FULL_NAME, 2, punct.rm=F, collapse=" ")
+df_parasit$SPECIES[119L] <- "insignipennis"
 
-idObs <- tmp[-id, ] %>% unique
-## TODO Mettre les individus pour Salix... :)
+## Issues ?
+# "Theroscopus hemipteron (Riche 1791) insignipennis (Schmiedeknecht 1905)"
+# "Saotis pygidiator pygidiator Kasparyan & Kopelke 2009"
+# Theroscopus insignipennis Schmiedeknecht 1905
+# "Kasparyan & Kopelke 2009"
 
+## add Authors
+df_salix$AUTHOR <- NA_character_
+df_parasit$AUTHOR <- keepWords(df_parasit$FULL_NAME, 3:8, punct.rm=F,
+  collapse=" ") %>% unlist %>% gsub(pat=" NA|\\(|\\)", rep="")
+df_parasit$AUTHOR[113L] <- "Kasparyan & Kopelke 2009"
+df_parasit$AUTHOR[119L] <- "Schmiedeknecht 1905"
+df_galler$AUTHOR %<>% gsub(pat="\\(|\\)", rep="")
 
+## Add Order and Genus
+##
+df_salix$ORDER <- "Malpighiales"
+df_salix$FAMILY <- "Salicaceae"
+df_salix$GENUS <- "Salix"
+##
+df_galler$ORDER <- "Hymenoptera"
+df_galler$FAMILY <- "Tenthredinidae"
 
-
-####----- interactPair
-
-# interactPair
-# df_interact (2 times!)
-rsal <- df_interact$RSAL %>% as.character
-rgal <- df_interact$RGALLER %>% as.character
-rpar <- df_interact$RPAR %>% as.character
-interactPair <- data.frame(
-  idFrom = c(rsal, rgal),
-  idTo = c(rgal, rpar),
-  stringsAsFactors = FALSE
-  )
-##-- Add frequence as value
-conc <- paste0(interactPair$idFrom, interactPair$idTo)
-tbint <- table(conc)
-# Unique pair of interactip
-interactPair <- unique(interactPair)
-interactPair$value <- NA
-for (i in 1:nrow(interactPair)){
-  tmpconc <- paste0(interactPair$idFrom[i],interactPair$idTo[i])
-  id <- which(names(tbint) == tmpconc)
-  interactPair$value[i] <- tbint[id]
-}
-interactPair <- interactPair[-which(interactPair$idTo == "none"),]
+## add a Type
+df_salix$TYPE <- "Salix"
+df_galler$TYPE <- "Galler"
+df_parasit$TYPE <- "Parasitoid"
 
 
-
-####----- INFO SITES
-sitetmp <- df_site2[,c(6,2:5,1)]
-## removing non-ascii character
-sitetmp$SITE <- stri_trans_general(sitetmp$SITE, "latin-ascii")
-sitetmp$REGION <- stri_trans_general(sitetmp$REGION, "latin-ascii")
-siteEnv <- melt(sitetmp, id.vars = names(sitetmp)[1])
-# names(siteEn)
+## Hybrids
+df_salix$HYBRIDS <- df_parasit$HYBRIDS <- df_galler$HYBRIDS <- FALSE
+df_salix$HYBRIDS[grepl(df_salix$SPECIES, pat=" x ")] <- TRUE
 
 
+## RESOLVED
+df_salix$RESOLVED <- df_parasit$RESOLVED <- df_galler$RESOLVED <- "Species"
+df_salix$RESOLVED[grepl(df_salix$SPECIES, pat="?", fixed=TRUE)] <- "Genus"
+##
+df_parasit$RESOLVED[grepl(df_salix$SPECIES, pat="?", fixed=TRUE)] <- "Genus"
+##
+df_parasit$RESOLVED[grepl(df_parasit$SPECIES, pat="sp[\\.1-9\\?]{1,2}")] <- "Genus"
+idb <- grepl(df_parasit$GENUS, pat="(blank)", fixed=TRUE)
+df_parasit$RESOLVED[idb] <- "Family"
+df_parasit$GENUS[idb] <- NA_character_
+# NB: id_row = 38 => superfamily
 
-####----- INFO SPECIES
-traiSP <- data.frame(
-  idSp = c(df_salix$RSAL %>% as.character,
-     df_galler$RGAL %>% as.character,
-     df_parasit$RPAR %>% as.character),
-  categ = c(rep("Salix", nrow(df_salix)),
-    rep("Galler", nrow(df_galler)),
-    rep("Parasitoid", nrow(df_parasit))),
-  genus = c(rep("Salix", nrow(df_salix)),
-    df_galler$GENUS,
-    df_parasit$GENUS),
-  species = c(df_salix$SPECIES %>% stri_trans_general("latin-ascii"),
-    df_galler$SPECIES %>% stri_trans_general("latin-ascii"),
-    df_parasit$FULL_NAME %>% keepWords(2) %>% unlist %>% stri_trans_general("latin-ascii")),
-  stringsAsFactors = FALSE
+##
+df_parasit$RESOLVED[grepl(df_galler$SPECIES, pat="sp\\.")] <- "Genus"
+df_galler$RESOLVED[grepl(df_galler$SPECIES, pat="\\?")] <- "Genus"
+
+##
+names(df_parasit)[1] <- "idNodes"
+names(df_galler)[1] <- "idNodes"
+names(df_salix)[1] <- "idNodes"
+names(df_galler)[7] <- "CODE_GALLTYPE_SIMPLE"
+names(df_parasit)[7:10] <- c("PAR_INQ", "ENDO_ECTO", "KOINO_IDIO", "TARGETED_LIFESTAGE")
+
+df_parasit$CODE_GALLTYPE_SIMPLE <- df_salix$CODE_GALLTYPE_SIMPLE <- NA_character_
+df_salix$PAR_INQ <- df_galler$PAR_INQ <- NA_character_
+df_salix$ENDO_ECTO <- df_galler$ENDO_ECTO <- NA_character_
+df_salix$KOINO_IDIO <- df_galler$KOINO_IDIO <- NA_character_
+df_salix$TARGETED_LIFESTAGE <- df_galler$TARGETED_LIFESTAGE <- NA_character_
+
+idnm <- c("idNodes", "ORDER", "FAMILY", "GENUS", "SPECIES", "RESOLVED", "AUTHOR",
+          "TYPE", "HYBRIDS", "CODE_GALLTYPE_SIMPLE", "PAR_INQ", "ENDO_ECTO", "KOINO_IDIO", "TARGETED_LIFESTAGE")
+
+####
+dfNodes <- rbind(
+  df_salix[idnm],
+  df_galler[idnm],
+  df_parasit[idnm]
   )
 
 
 
+######################### Building dfSite
+dfSite <- df_site[, c('SITE', 'NDECDEG', 'EDECDEG', 'COUNTRY', 'REGION')] %>% unique
+dfSite$idSite <- paste0("site", sprintf("%03d", 1:nrow(df_site2)))
+rownames(dfSite) <- NULL
+dfSite <- dfSite[, c(6,1:3,5,4)]
+#### add idSite to df_site
 
-####----- SAVING THA DATA
-kopelke <- alien::as.alienData(idObs, interactPair, siteEnv = siteEnv, traitSp = traiSP)
+df_site$idSite <- df_intract$idSite <- ""
+for (i in 1:nrow(df_site)) {
+  sco <- (dfSite[, 'SITE'] == df_site[i, 'SITE']) +
+    (dfSite[, 'NDECDEG'] == df_site[i, 'NDECDEG']) +
+    (dfSite[, 'EDECDEG'] == df_site[i, 'EDECDEG'])
+  # print(which(sco==3))
+  df_site$idSite[i] <- dfSite$idSite[which(sco==3)]
+}
+##
+for (i in 1:nrow(df_intract)) {
+  id <- which(df_site$REARING_NUMBER == df_intract$REARING_NUMBER[i])
+  df_intract$idSite[i] <- df_site$idSite[id]
+}
+
+
+
+######################### Building dfEdges
+
+aggregate(NB_GALLS_PAR ~ REARING_NUMBER*RSAL*RGALLER, data = df_intract, sum)
+
+
+int_gal <- df_intract[, c("RSAL", "RGALLER", "N_GALLS", "idSite")] %>% unique
+int_par <- df_intract[, c("RGALLER", "RPAR", "NB_GALLS_PAR", "idSite")] %>% unique
+
+int_par <- int_par[ -which(int_par$RPAR=="none"),]
+
+names(int_gal)[1:3] <- c("idFrom", "idTo", "value")
+names(int_par)[1:3] <- c("idFrom", "idTo", "value")
+
+dfEdges <- rbind(int_gal, int_par)
+
+
+
+######################### SAVING THA DATA
+dfSite$SITE %<>% stri_trans_general("latin-ascii")
+dfSite$REGION %<>% stri_trans_general("latin-ascii")
+kopelke <- alienData(dfNodes, dfEdges, trait = 7:13, taxo = c(2:6), dfSite = dfSite)
 save(kopelke, file='kopelke.rda', compress='xz', ascii=TRUE)
-# devtools::use_data(sal, salix)
-# save(salix, file="salix.Rda")
-# save(bartomeus, file='bartomeus.rda', compress='xz')
+# # devtools::use_data(sal, salix)
+# # save(salix, file="salix.Rda")
+# # save(bartomeus, file='bartomeus.rda', compress='xz')
